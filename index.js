abcc3bf6f5b5b276524b1b7fc7957b8c074eb91a
Luis Cedillo <luis.m.cedillo@gmail.com>
2018-08-06 01:47:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,30 @@ $(document).ready(function() {
         $('#removeLink').show();
       };
   });
-
 });
+
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = 'https://github.com/luis-cedillo/portfolio/blob/gh-pages/images/me%201.png?raw=true';
+images[1] = 'https://github.com/luis-cedillo/portfolio/blob/gh-pages/images/me%202.jpg?raw=true';
+images[2] = 'https://github.com/luis-cedillo/portfolio/blob/gh-pages/images/me%203.jpg?raw=true';
+images[3] = 'https://github.com/luis-cedillo/portfolio/blob/gh-pages/images/me%204.jpg?raw=true';
+images[4] = 'https://github.com/luis-cedillo/portfolio/blob/gh-pages/images/me%205.jpg?raw=true';
+images[5] = 'https://github.com/luis-cedillo/portfolio/blob/gh-pages/images/me%206.jpg?raw=true';
+
+function changeImg(){
+  document.slide.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  }
+  else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
